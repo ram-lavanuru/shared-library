8b2b087a1c49a8eddaf0777a0187387aa739aeba
Lavanuru <v.lavanuru@dxc.com>
2024-09-23 07:20:33 +0530

    calculator commited

---
+++ src/com/rr/builds/Calculator.groovy
@@ -0,0 +1,20 @@
+package com.rr.builds;
+
+
+class Calculator {
+    def jenkins
+    Calucalator (jenkins) {
+        this.jenkins = jenkins
+    }
+}
+
+
+def add(firstNumber, secondNumber) {
+
+    return firstNumber+secondNumber
+}
+
+
+def multiply(firstNumber, secondNumber) {
+    return firstNumber*secondNumber
+}
